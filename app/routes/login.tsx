2d6e35ad0725e5d48f2959217f79c1b55021d350
Jaime Cervantes <jaime.cervantes.ve@gmail.com>
2023-06-24 14:46:01 -0600

    refactor: extract login route code into functions and useLogin custom hook

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,126 +1,36 @@
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import {
-  Link,
-  useActionData,
-  useLoaderData,
-  useSubmit,
-} from "@remix-run/react";
-import { useCallback, useState } from "react";
+import { Link, useActionData, useLoaderData } from "@remix-run/react";
+import { getSession } from "~/sessions";
+import { getSessionCookieHeader, login } from "~/functions/login";
 
-import * as firebaseRest from "~/firebase/firebase-rest";
-import {
-  checkSessionCookie,
-  signIn,
-  signInWithToken,
-} from "~/firebase/auth.server";
+import { checkSessionCookie } from "~/firebase/auth.server";
 import { getRestConfig } from "~/firebase/firebase.server";
-import { commitSession, getSession } from "~/sessions";
-import app from "~/firebase/firebase.client";
-import * as auth from "firebase/auth";
-import type { UserCredential } from "firebase/auth";
 import TextField from "~/components/TextField/TextField";
+import useLogin from "~/functions/useLogin";
 
-export const loader = async ({ request }: LoaderArgs) => {
-  const session = await getSession(request.headers.get("cookie"));
+export async function loader({ request }: LoaderArgs) {
+  const cookie = request.headers.get("cookie");
+  const session = await getSession(cookie);
   const { uid } = await checkSessionCookie(session);
-  const headers = {
-    "Set-Cookie": await commitSession(session),
-  };
+  const headers = await getSessionCookieHeader(cookie);
   if (uid) {
     return redirect("/", { headers });
   }
+
   const { apiKey, domain } = getRestConfig();
   return json({ apiKey, domain }, { headers });
-};
+}
 
-type ActionData = {
-  error?: string;
-};
-export const action = async ({ request }: ActionArgs) => {
-  const form = await request.formData();
-  const idToken = form.get("idToken");
-  let sessionCookie;
-  try {
-    if (typeof idToken === "string") {
-      sessionCookie = await signInWithToken(idToken);
-    } else {
-      const email = form.get("email");
-      const password = form.get("password");
-      const formError = json(
-        { error: "¡Por favor, llena todos los campos!" },
-        { status: 400 }
-      );
-      if (typeof email !== "string") return formError;
-      if (typeof password !== "string") return formError;
-      sessionCookie = await signIn(email, password);
-    }
-    const session = await getSession(request.headers.get("cookie"));
-    session.set("session", sessionCookie);
-    return redirect("/", {
-      headers: {
-        "Set-Cookie": await commitSession(session),
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    return json({ error: String(error) }, { status: 401 });
-  }
-};
+export async function action({ request }: ActionArgs) {
+  return await login(request);
+}
 
 export default function Login() {
-  const [clientAction, setClientAction] = useState<ActionData>();
   const actionData = useActionData<typeof action>();
   const restConfig = useLoaderData<typeof loader>();
-  const submit = useSubmit();
-
-  const handleSubmit = useCallback(
-    async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      // To avoid rate limiting, we sign in client side if we can.
-      const login = await firebaseRest.signInWithPassword(
-        {
-          email: event.currentTarget.email.value,
-          password: event.currentTarget.password.value,
-          returnSecureToken: true,
-        },
-        restConfig
-      );
-      if (firebaseRest.isError(login)) {
-        setClientAction({ error: login.error.message });
-        return;
-      }
-      submit({ idToken: login.idToken }, { method: "post" });
-    },
-    [submit, restConfig]
-  );
-
-  async function onProviderSignIn(credentials: UserCredential) {
-    const idToken = await credentials.user.getIdToken();
-    submit({ idToken }, { method: "post" });
-  }
-
-  function signInWithGoogle() {
-    const clientAuth = auth.getAuth(app);
-    auth
-      .signInWithPopup(clientAuth, new auth.GoogleAuthProvider())
-      .then(onProviderSignIn)
-      .catch((error: Error) => {
-        console.log(error);
-        alert(error.message);
-      });
-  }
-
-  function signInWithFacebook() {
-    const clientAuth = auth.getAuth(app);
-    auth
-      .signInWithPopup(clientAuth, new auth.FacebookAuthProvider())
-      .then(onProviderSignIn)
-      .catch((error: Error) => {
-        console.log(error);
-        alert(error.message);
-      });
-  }
+  const { handleSubmit, loginWithGoogle, loginWithFacebook, clientAction } =
+    useLogin(restConfig);
 
   return (
     <div>
@@ -134,23 +44,21 @@ export default function Login() {
           name="email"
           placeholder="tu@pensemosweb.com"
           type="email"
-        >
-        </TextField>
+        ></TextField>
 
         <TextField
           label="Contraseña"
           name="password"
           placeholder="Tu contraseña"
           type="password"
-        >
-        </TextField>
+        ></TextField>
 
         <button style={{ display: "block" }} type="submit">
           Iniciar sesión
         </button>
       </form>
-      <button onClick={signInWithGoogle}>Entrar con google</button>
-      <button onClick={signInWithFacebook}>Entrar con Facebook</button>
+      <button onClick={loginWithGoogle}>Entrar con google</button>
+      <button onClick={loginWithFacebook}>Entrar con Facebook</button>
 
       <p>
         <Link to="/join">Registrarme</Link>
