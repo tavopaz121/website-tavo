b16a4b78fbaede0776462b6b8157e49c79ef2a97
tavopaz121 <tavo.paz12@hotmail.com>
2023-07-11 15:17:29 -0600

    buil: add palette of colors in file tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,17 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pw-green': '#2ABF40',
+        'pw-lightgreen': '#5DBF17',
+        'pw-lightorange': '#F2B705',
+        'pw-orange': '#F28705',
+        'pw-white': '#F2F2F2',
+        'pw-gray': '#D9D9D9',
+        'pw-black': '#262626',
+      },
+    },
   },
   plugins: [],
 } satisfies Config
-
