3ccf562d84c82020e7b65e6ab91c980c92d8c941
CascoArcilla <normon1230@gmail.com>
2023-07-11 11:38:32 -0600

    build: add fonts and sizes to tailwind.congin.ts

--- app/components/PwLogin/HeaderLogin.jsx
+++ app/components/PwLogin/HeaderLogin.jsx
@@ -1,15 +1,15 @@
 export default function HeaderLogin() {
-  const headerStyles = `header rounded-t-lg relative mb-6 bg-slate-300 -mx-12 -mt-5 px-8 pt-4 max-[640px]:-mx-6 max-[640px]:px-6`;
+  const headerStyles = `rounded-t-lg relative mb-6 bg-slate-300 -mx-12 -mt-5 px-8 pt-4 max-[640px]:-mx-6 max-[640px]:px-6`;
   return (
     <div
       className={headerStyles}
     >
       <div
-        className="title-form text-3xl font-bold text-center"
+        className="text-center font-poppins text-tt32 font-bold"
       >
-        Inicia sesión
+        INICIAR SESIÓN
       </div>
-      <p data-testid="description" className="description-form text-justify">
+      <p data-testid="description" className="text-justify font-lato text-pg16">
         Bienvenido al inicio de sesión de Healty Food, aplicación local para ayudar a promover una buena salud a través de los alimentos.
       </p>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,20 @@ export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      'monserrat': ['Montserrat', 'sans-serif'],
+      'crimson': ['Crimson Text', 'serif'],
+      'roboto': ['Roboto', 'sans-serif'],
+      'poppins': ['Poppins', 'sans-serif'],
+      'lato': ['Lato', 'sans-serif']
+    },
+    fontSize: {
+      'tt32': '32px',
+      'tt24': '24px',
+      'tt20': '20px',
+      'pg16': '16px',
+      'pg14': '14px',
+    }
   },
   plugins: [],
 } satisfies Config
