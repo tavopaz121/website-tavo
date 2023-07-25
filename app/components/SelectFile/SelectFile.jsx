c1f67a20b460c80dfe0b72f7b8ce7072ef62a759
tavopaz121 <tavo.paz12@hotmail.com>
2023-07-25 12:43:05 -0600

    refactor: resolve conflicts

--- app/components/SelectFile/SelectFile.jsx
+++ app/components/SelectFile/SelectFile.jsx
@@ -14,11 +14,13 @@ export default function SelectFile({
       {title && <p className="block text-lg mb-1 font-medium">{title}</p>}
 
       <div
-        className={`flex gap-4 w-full align-middle bg-white border border-black rounded-md ${className}`}>
+        className={`flex gap-4 w-full align-middle bg-white border border-black rounded-md ${className}`}
+      >
         <label
           data-testid="label-file"
           htmlFor="file"
-          className="w-full flex gap-4 p-3 cursor-pointer">
+          className="w-full flex gap-4 p-3 cursor-pointer"
+        >
           <UploadIcon fill="#262626" />
           <span className="mt-1 text-lg font-medium leading-normal text-pw-black">
             {placeholder}
