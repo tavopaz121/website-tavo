cc534ab3e66565e4adbe10b4d5df2ee034fb89d0
CascoArcilla <normon1230@gmail.com>
2023-11-15 10:37:44 -0600

    refactor:HomePage altered text size titles in hero section

--- app/components/pages/home/Hero.tsx
+++ app/components/pages/home/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
 
       <div className="absolute top-0 h-[100vb] text-white w-full flex justify-center items-start flex-col mx-auto p-6">
         <span
-          className="motion-safe:animate-fadeInDown text-4xl lg:text-5xl font-bold mb-6 lg:mb-11"
+          className="motion-safe:animate-fadeInDown max-[375px]:text-3xl text-4xl lg:text-5xl font-bold mb-6 lg:mb-11"
           style={{
             animationDelay: "1s",
             animationFillMode: "both",
@@ -22,7 +22,7 @@ export default function Hero() {
           </mark>
         </span>
         <span
-          className="motion-safe:animate-fadeInUp font-bold text-5xl lg:text-7xl mb-5 tracking-tighter"
+          className="motion-safe:animate-fadeInUp font-bold max-[375px]:text-4xl text-5xl lg:text-7xl mb-5 tracking-tighter"
           style={{
             animationDelay: "1.5s",
             animationFillMode: "both",
@@ -33,7 +33,7 @@ export default function Hero() {
           </mark>
         </span>
         <span
-          className="motion-safe:animate-fadeInLeft font-bold text-center text-2xl lg:text-3xl lg:mt-3"
+          className="motion-safe:animate-fadeInLeft font-bold text-center max-[375px]:text-xl text-2xl lg:text-3xl lg:mt-3"
           style={{
             animationDelay: "2s",
             animationFillMode: "both",
