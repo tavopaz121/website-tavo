37be083aa8be73556137c2452359d7ae7111c9ab
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2023-11-16 13:03:22 -0600

    refactor: try a differnt layout for home services

--- app/components/pages/home/Servicios.tsx
+++ app/components/pages/home/Servicios.tsx
@@ -1,4 +1,5 @@
 import ListServicios from "~/components/HomePage/ListServicios";
+import servicesImg from "~/assets/imgs/inicio/services.webp";
 
 export default function SectionServicios() {
   return (
@@ -8,11 +9,17 @@ export default function SectionServicios() {
           <span className="inline-block py-1 px-3 mb-4 text-xs font-semibold text-pink-500 bg-orange-50 rounded-full">
             SERVICIOS
           </span>
-          <h2 className="font-heading text-5xl xs:text-6xl md:text-7xl font-bold text-gray-900 mb-24">
-            <span>Ofrecemos nuestros </span>
+          <h2 className="font-heading text-5xl xs:text-6xl md:text-7xl font-bold text-gray-900 mb-8">
+            <span>Nuestros </span>
             <span className="font-serif italic">servicios</span>
           </h2>
         </div>
+        <div
+          className="rounded-[50%] w-[600px] h-[600px] bg-cover bg-center bg-no-repeat mx-auto mb-8"
+          style={{
+            backgroundImage: `url(${servicesImg})`,
+          }}
+        ></div>
         <ListServicios />
       </div>
     </section>
