9b7b6fcf7bbb1f1c183ead29f737d8ab962f203f
CascoArcilla <normon1230@gmail.com>
2023-11-24 13:25:56 -0600

    Refactor: Added animations Equipo section in Home page

--- app/components/pages/home/Equipo.tsx
+++ app/components/pages/home/Equipo.tsx
@@ -5,7 +5,13 @@ export default function Equipo() {
   return (
     <section className="py-20 md:py-24">
       <div className="container px-20 max-lg:px-0 mx-auto">
-        <div className="relative max-w-7xl mx-auto px-6 max-xl:px-0 py-16 max-xl:pt-0 rounded-4xl max-xl:rounded-none bg-gray-900 overflow-hidden flex flex-row-reverse max-xl:flex-col items-center">
+        <div
+          className="relative max-w-7xl mx-auto px-6 max-xl:px-0 py-16 max-xl:pt-0 rounded-4xl max-xl:rounded-none bg-gray-900 overflow-hidden flex flex-row-reverse max-xl:flex-col items-center motion-safe:animate-fadeInLeft"
+          style={{
+            animationDelay: `0s`,
+            animationFillMode: "both",
+          }}
+        >
           <img
             className="absolute top-0 left-1/2 transform -translate-x-1/2 h-full object-cover"
             src={circle}
@@ -14,19 +20,39 @@ export default function Equipo() {
           <img
             src={equipo}
             alt="Un equipo trabajando"
-            className="absolute max-xl:static top-0 left-0 transform translate-x-0 h-full max-xl:h-auto max-xl:w-full object-cover"
+            className="absolute max-xl:static top-0 left-0 transform translate-x-0 h-full max-xl:h-auto max-xl:w-full object-cover motion-safe:animate-fadeInRight"
+            style={{
+              animationDelay: `0.4s`,
+              animationFillMode: "both",
+            }}
           />
 
           <div className="relative text-right max-xl:text-center w-[50%] max-xl:w-3/4 max-xl:mt-8 max-xl:flex flex-col items-center">
-            <span className="block text-2xl sm:text-3xl md:text-4xl font-semibold text-gray-600 mb-6">
+            <span
+              className="block text-2xl sm:text-3xl md:text-4xl font-semibold text-gray-600 mb-6 motion-safe:animate-fadeInLeft"
+              style={{
+                animationDelay: `0.6s`,
+                animationFillMode: "both",
+              }}
+            >
               ¿Listo para la colaboracion?
             </span>
-            <h2 className="font-heading text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-8">
+            <h2
+              className="font-heading text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-8 motion-safe:animate-fadeInLeft"
+              style={{
+                animationDelay: `1s`,
+                animationFillMode: "both",
+              }}
+            >
               <span>Conoce a nuestro equipo de trabajo</span>
               <span className="font-serif italic"></span>
             </h2>
             <a
-              className="relative group inline-block w-full max-xl:w-3/4 sm:w-auto py-4 px-6 text-blue-900 hover:text-white font-semibold bg-gray-100 rounded-md overflow-hidden transition duration-300"
+              className="relative group inline-block w-full max-xl:w-3/4 sm:w-auto py-4 px-6 text-blue-900 hover:text-white font-semibold bg-gray-100 rounded-md overflow-hidden transition duration-300 motion-safe:animate-fadeIn"
+              style={{
+                animationDelay: `1.4s`,
+                animationFillMode: "both",
+              }}
               href="/equipo"
             >
               <div className="absolute top-0 right-full w-full h-full bg-gradient-pink transform group-hover:translate-x-full group-hover:scale-110 transition duration-500" />
