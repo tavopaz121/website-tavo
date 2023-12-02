11dbdde278f2b828498d2630cd804125b2690329
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2023-12-02 00:38:51 -0600

    refactor: remove inicio item from nav

--- app/data/navItems.js
+++ app/data/navItems.js
@@ -1,5 +1,4 @@
 export const items = [
-  { to: "/", label: "Inicio" },
   { to: "/servicios", label: "Servicios" },
   { to: "/mentorias1a1", label: "Mentorias1a1" },
   { to: "/proyectos", label: "Proyectos" },
