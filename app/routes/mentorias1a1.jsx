da80614af68ddb6b590741f043933b3cbc4c83d0
Nestor <nestor.gh22@hotmail.com>
2023-12-05 17:07:50 -0600

    feat: mentories cards v1

--- app/routes/mentorias1a1.jsx
+++ app/routes/mentorias1a1.jsx
@@ -1,6 +1,7 @@
 import metodoAgile from "../assets/imgs/clasesonetoone/feedback_loo_transparent_noTitle.webp";
 import dontLoseFocus from "~/assets/svgs/dontLoseFocus.svg";
 import sectionScrum from "~/assets/imgs/clasesonetoone/section_scrum.webp";
+import picturejs from "~/assets/imgs/clasesonetoone/picture-js.png"; 
 import { items as principles } from "~/data/principles.js";
 
 import AOS from "aos";
@@ -109,6 +110,143 @@ export default function Mentorias() {
           </div>
         </div>
       </section>
+
+      {/* Mentorias / Cursos  */}
+      <section>
+        <div className=" max-w-6xl grid grid-cols-12 gap-6 px-4 mx-auto">
+          {/* Cards a */}
+          <div className="flex flex-col items-center col-span-full xl:col-span-8 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
+            <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
+              <h2 className="font-semibold text-slate-800 dark:text-slate-100">
+                JavaScript Frontend
+              </h2>
+            </header>
+            <div className="px-5 py-1">
+              <div className="flex flex-wrap ">
+                {/* Unique Visitors */}
+                <div className="flex items-center py-2">
+                  <div className="text-center">
+                    <div>
+                      <div className="pb-5">
+                        <img
+                          className="mx-auto"
+                          src={picturejs}
+                          width={200}
+                          height={300}
+                          alt="JavaScript Frontend"
+                        />
+                      </div>
+                    </div>
+                    <div className="text-justify text-white">
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas adipisci ipsam id alias unde aliquam minus et accusamus assumenda harum expedita saepe necessitatibus rerum in, dicta libero nemo deleniti inventore?
+                      </p>
+                    </div>
+                    <a
+                      className="btn text-white bg-gray-700 hover:bg-gray-800 lg:w-full w-auto mx-auto"
+                      href="#principios"
+                    >
+                      Información
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Cards b */}
+          <div className="flex flex-col items-center col-span-full xl:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
+            <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
+              <h2 className="font-semibold text-slate-800 dark:text-slate-100">
+                JavaScript Frontend
+              </h2>
+            </header>
+            <div className="px-5 py-1">
+              <div className="flex flex-wrap ">
+                {/* Unique Visitors */}
+                <div className="flex items-center py-2">
+                  <div className="text-center">
+                    <div>
+                      <div className="pb-5">
+                        <img
+                          className="mx-auto"
+                          src={picturejs}
+                          width={200}
+                          height={300}
+                          alt="JavaScript Frontend"
+                        />
+                      </div>
+                    </div>
+                    <div className="text-center lg:text-justify text-white pb-5">
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas adipisci ipsam id alias unde aliquam minus et accusamus assumenda harum expedita saepe necessitatibus rerum in, dicta libero nemo deleniti inventore?
+                      </p>
+                    </div>
+                    <ul className="flex flex-col items-start space-y-5 text-left text-gray-300 mb-2">
+                      <li className="flex items-center mx-2 my-1">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Simple Design</span>
+                      </li>
+                      <li className="flex items-center mx-2 my-1">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Mob Programming</span>
+                      </li>
+                      <li className="flex items-center mx-2 my-1">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>Pair Programming</span>
+                      </li>
+                      <li className="flex items-center mx-2 my-1">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>BDD</span>
+                      </li>
+                      <li className="flex items-center mx-2 my-1">
+                        <svg
+                          className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                        </svg>
+                        <span>TDD</span>
+                      </li>
+                    </ul>
+                    <a
+                      className="btn text-white bg-gray-700 hover:bg-gray-800 lg:w-full w-auto mx-auto"
+                      href="#principios"
+                    >
+                      Obtener
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
       {/* Principios Ágiles */}
       <section id="principios">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -284,7 +422,7 @@ export default function Mentorias() {
                         agilizar e interiorizar lo aprendido.
                       </p>
                     </div>
-                    <ul className="flex flex-wrap justify-center lg:justify-start mt-4 text-lg text-gray-400 -mx-2 -my-1">
+                    <ul className="flex flex-col items-start space-y-5 text-left text-gray-300 mt-9">
                       <li className="flex items-center mx-2 my-1">
                         <svg
                           className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
