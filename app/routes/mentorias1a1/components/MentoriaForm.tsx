e5991e0e39c38a02fcc83fb4eb51ae762374f1cb
CascoArcilla <normon1230@gmail.com>
2023-12-13 15:05:11 -0600

    Refactor:wrote the names of the fields in form mentorias

--- app/routes/mentorias1a1/components/MentoriaForm.tsx
+++ app/routes/mentorias1a1/components/MentoriaForm.tsx
@@ -1,4 +1,9 @@
 import { Form } from "@remix-run/react";
+import stylesForm from "../styles.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: stylesForm, content: "text/css" }];
+}
 
 export default function MentoriaForm() {
   return (
@@ -23,7 +28,11 @@ export default function MentoriaForm() {
             </p>
           </div>
 
-          <Form className="max-w-xl mx-auto">
+          <Form
+            className="max-w-2xl sm:p-5 mx-auto"
+            data-aos="fade-left"
+            data-aos-delay="400"
+          >
             <div className="flex flex-wrap -mx-3 mb-4">
               <div className="w-full md:w-1/2 px-3 mb-4 md:mb-0">
                 <label
@@ -36,7 +45,7 @@ export default function MentoriaForm() {
                   id="first-name"
                   type="text"
                   className="form-input bg-black p-4 w-full text-white focus:outline-none focus:border-[1px] focus:border-pink-600 placeholder-slate-400"
-                  placeholder="Enter your first name"
+                  placeholder="Introcude tu nombre"
                   required
                 />
                 <p className="text-red-500 text-sm mt-2">
@@ -54,7 +63,7 @@ export default function MentoriaForm() {
                   id="last-name"
                   type="text"
                   className="form-input bg-black p-4 w-full text-white focus:outline-none focus:border-[1px] focus:border-pink-600 placeholder-slate-400"
-                  placeholder="Enter your last name"
+                  placeholder="Coloca tu apellido"
                   required
                 />
               </div>
@@ -71,7 +80,7 @@ export default function MentoriaForm() {
                   id="email"
                   type="email"
                   className="form-input bg-black p-4 w-full text-white focus:outline-none focus:border-[1px] focus:border-pink-600 placeholder-slate-400"
-                  placeholder="Enter your email address"
+                  placeholder="Tu email va aquí"
                   required
                 />
               </div>
@@ -82,14 +91,13 @@ export default function MentoriaForm() {
                   className="block text-gray-200 text-base font-normal mb-1"
                   htmlFor="subject"
                 >
-                  Telefono <span className="text-red-600">*</span>
+                  Telefono
                 </label>
                 <input
                   id="subject"
                   type="number"
                   className="form-input bg-black p-4 w-full text-white focus:outline-none focus:border-[1px] focus:border-pink-600 placeholder-slate-400"
-                  placeholder="How can we help you?"
-                  required
+                  placeholder="Tu número de telefono"
                 />
               </div>
             </div>
@@ -99,18 +107,23 @@ export default function MentoriaForm() {
                   className="block text-gray-200 text-base font-normal mb-1"
                   htmlFor="country"
                 >
-                  Tema de interes
+                  Tema de interes <span className="text-red-600">*</span>
                 </label>
                 <select
                   id="country"
                   className="form-select bg-black p-4 w-full text-white focus:outline-none focus:border-[1px] focus:border-pink-600 placeholder-slate-400"
                 >
-                  <option value="">Agile Frontend Developer</option>
-                  <option value="">Agile Node.js Developer</option>
-                  <option value="">Agile</option>
-                  <option value="">HTML 5</option>
-                  <option value="">CSS 3</option>
-                  <option value="">JavaScript</option>
+                  <option value="none">Elije el tema que te interese</option>
+                  <option value="agile-frontend-developer">
+                    Agile Frontend Developer
+                  </option>
+                  <option value="agile-node.js-developer">
+                    Agile Node.js Developer
+                  </option>
+                  <option value="agile">Agile</option>
+                  <option value="html5">HTML 5</option>
+                  <option value="css3">CSS 3</option>
+                  <option value="javascript">JavaScript</option>
                 </select>
               </div>
             </div>
@@ -120,22 +133,22 @@ export default function MentoriaForm() {
                   className="block text-gray-200 text-base font-normal mb-1"
                   htmlFor="message"
                 >
-                  Message
+                  Mensaje
                 </label>
                 <textarea
                   id="message"
                   rows={4}
-                  className="form-textarea bg-black p-4 w-full text-slate-400 focus:outline-none focus:border-[1px] focus:border-pink-600"
-                  placeholder="Write your message"
+                  className="form-textarea bg-black p-4 w-full text-white focus:outline-none focus:border-[1px] focus:border-pink-600"
+                  placeholder="Cuentanos tu interes o lo que quieres saber"
                 ></textarea>
               </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-4">
               <div className="w-full px-3">
                 <label className="flex items-center">
-                  <input type="checkbox" className="form-checkbox" />
-                  <span className="text-gray-400 ml-2">
-                    I agree to the privacy policy
+                  <input type="checkbox" className="form-checkbox" required />
+                  <span className="text-gray-400 font-normal ml-2">
+                    Acepto las politicas de privacidad
                   </span>
                 </label>
               </div>
@@ -143,7 +156,7 @@ export default function MentoriaForm() {
             <div className="flex flex-wrap -mx-3 mt-6">
               <div className="w-full px-3">
                 <button className="btn text-white bg-gradient-pink hover:opacity-80 lg:w-full w-auto mx-auto">
-                  Send
+                  Enviar
                 </button>
               </div>
             </div>
