ac3c85d7149f0ed6419aba6a184daa6fbe6b39fb
CascoArcilla <normon1230@gmail.com>
2023-12-24 13:51:40 -0600

    Refactor:made layault blog in home

---
+++ app/routes/_index/data/postsHome.js
@@ -0,0 +1,41 @@
+import imageBlog from "../imgs/corazon-agilidad.webp";
+import imageBlog2 from "../imgs/individuosEInteracciones.webp";
+import imageBlog3 from "../imgs/Portada-IntroLEanStartup-.webp";
+
+export const blogs = [
+  {
+    image: imageBlog,
+    fecha: "Oct 02, 2019",
+    titulo:
+      "El manifiesto ágil, siempre he pensado que las cosas simples son las mejores.",
+    anchor: "https://pensemosweb-mx.web.app/el-corazon-agil-manifiesto/",
+  },
+  {
+    image: imageBlog2,
+    fecha: "Ago 18, 2019",
+    titulo: "¿CÓMO SURGIÓ EL DESARROLLO ÁGIL? PRINCIPIOS FUNDAMENTALES",
+    anchor:
+      "https://pensemosweb-mx.web.app/como-surgio-el-desarrollo-agil-principios-fundamentales/",
+  },
+  {
+    image: imageBlog3,
+    fecha: "Dic 29, 2018",
+    titulo:
+      "Introducción Lean Startup, utilización del conocimiento y creatividad de los individuos.",
+    anchor: "https://pensemosweb-mx.web.app/introduccion-lean-startup/",
+  },
+  {
+    image: imageBlog3,
+    fecha: "Dic 29, 2018",
+    titulo:
+      "Introducción Lean Startup, utilización del conocimiento y creatividad de los individuos.",
+    anchor: "https://pensemosweb-mx.web.app/introduccion-lean-startup/",
+  },
+  {
+    image: imageBlog3,
+    fecha: "Dic 29, 2018",
+    titulo:
+      "Introducción Lean Startup, utilización del conocimiento y creatividad de los individuos.",
+    anchor: "https://pensemosweb-mx.web.app/introduccion-lean-startup/",
+  },
+];
