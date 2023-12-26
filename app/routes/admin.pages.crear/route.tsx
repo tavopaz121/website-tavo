1c8df27e4b7dbfc53a971400ba7fa40013f7d28d
tavopaz121 <tavo.paz12@hotmail.com>
2023-12-26 15:21:15 -0600

    refactor: add form in route admin/pages/crear

--- app/routes/admin.pages.crear/route.tsx
+++ app/routes/admin.pages.crear/route.tsx
@@ -4,32 +4,56 @@ import TextField from "~/components/AdminPages/TextField/TextField";
 import TextArea from "~/components/AdminPages/TextArea/TextArea";
 
 export default function CrearPages() {
-  const [metas, setMetas] = useState([
-    {
-      name: "og:locale",
-      content: "es-MX",
-    },
-    {
-      name: "og:type",
-      content: "website",
-    },
-    {
-      name: "og:title",
-      content: "",
-    },
-    {
-      name: "og:description",
-      content: "",
-    },
-    {
-      name: "og:url",
-      content: "",
-    },
-    {
-      name: "og:site_name",
-      content: "",
-    },
-  ]);
+  const [metas, setMetas] = useState({
+    openGraph: [
+      {
+        name: "og:locale",
+        content: "es-MX",
+      },
+      {
+        name: "og:type",
+        content: "website",
+      },
+      {
+        name: "og:title",
+        content: "",
+      },
+      {
+        name: "og:description",
+        content: "",
+      },
+      {
+        name: "og:url",
+        content: "",
+      },
+      {
+        name: "og:site_name",
+        content: "",
+      },
+    ],
+    twitter: [
+      {
+        name: "twitter:card",
+        content: "",
+      },
+      {
+        name: "twitter:site",
+        content: "",
+      },
+      {
+        name: "twitter:creator",
+        content: "",
+      },
+      {
+        name: "twitter:title",
+        content: "",
+      },
+      {
+        name: "twitter:description",
+        content: "",
+      },
+    ],
+  });
   return (
     <section className="grid place-items-center items-start my-10">
       <div className="w-full lg:w-[60%]">
@@ -74,7 +98,7 @@ export default function CrearPages() {
               <span className="text-indigo-500">2.</span> Open Graph
             </div>
             <div className="space-y-4">
-              {metas.map((meta, index) => (
+              {metas.openGraph?.map((meta, index) => (
                 <div className="w-full mb-4 md:mb-0" key={index}>
                   <TextField
                     title={meta.name}
@@ -84,6 +108,35 @@ export default function CrearPages() {
                   />
                 </div>
               ))}
+            </div>
+          </div>
+
+          <div className="py-6">
+            <div className="text-lg font-bold text-white mb-5">
+              <span className="text-indigo-500">3.</span> Twitter
+            </div>
+            <div className="space-y-4">
+              {metas.twitter?.map((meta, index) => (
+                <div className="w-full mb-4 md:mb-0" key={index}>
+                  <TextField
+                    title={meta.name}
+                    name="title"
+                    placeholder={`Ingresa el '${meta.name}' de la aplicación`}
+                    value={meta.content}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-wrap -mx-3 mt-6">
+            <div className="w-full px-3">
+              <button
+                type="submit"
+                className="btn text-white bg-gradient-pink hover:opacity-80 w-full mx-auto"
+              >
+                Enviar
+              </button>
             </div>
           </div>
         </Form>
